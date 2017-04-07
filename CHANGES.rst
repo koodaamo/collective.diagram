1010f3045bb137c33e35f24014b4b6519bafa084
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-04-07 13:28:40 +0300

    release 1.0rc0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+1.0rc0 (2017-04-07)
+-------------------
+
+- First release candidate
 
 1.0a1 (unreleased)
 ------------------
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.diagram',
-    version='1.0a1',
+    version='1.0rc0',
     description="Plone add-on to render diagrams from source markup",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
