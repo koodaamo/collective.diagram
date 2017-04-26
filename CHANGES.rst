796d0ec69e110fc0b8e854b9a76963daa2d1b22a
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-04-26 23:17:57 +0300

    release rc1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,13 @@
 Changelog
 =========
 
+
+1.0rc1 (2017-04-26)
+-------------------
+
+- Switch to using schema instead of model
+- Added requirements
+
 1.0rc0 (2017-04-07)
 -------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.diagram',
-    version='1.0rc0',
+    version='1.0rc1',
     description="Plone add-on to render diagrams from source markup",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
